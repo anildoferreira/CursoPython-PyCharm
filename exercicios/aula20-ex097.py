9f77f388dcf7f41228a8724644f05142ff212b9f
anildoferreira <ferreiraanildo@gmail.com>
2020-07-08 11:20:53 -0300

    Exercícios Python

---
+++ exercicios/aula20-ex097.py
@@ -0,0 +1,12 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+# Programa principal
+escreva('Anildo Ferreira')
+escreva('Curso de Python')
+escreva('Eu ficando profissional')
+escreva('Nil')
